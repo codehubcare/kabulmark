f0347b40e3b92a87f708282bdc8b6180c81e9076
Shamshad Zaheer <shamshadzr@gmail.com>
2025-07-18 23:48:21 -0700

    Fix JSX issues: Add proper TypeScript config and rename file to .tsx

---
+++ src/index.tsx
@@ -0,0 +1,32 @@
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+
+function Editor() {
+  return (
+    <LexicalComposer
+      initialConfig={{
+        namespace: "MyEditor",
+        theme: {
+          text: {
+            bold: "font-bold"
+          }
+        },
+        onError: (error: Error) => {
+          console.error(error);
+        }
+      }}
+    >
+      <RichTextPlugin
+        contentEditable={<ContentEditable />}
+        placeholder={<div>Enter some text...</div>}
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+      <HistoryPlugin />
+    </LexicalComposer>
+  );
+}
+
+export default Editor;
